89f389ec988b316b53eb6710495cf0c8588ce219
matthew <matthew@epiphanydesign.co>
2014-04-08 07:23:38 -0500

    Revert "txp setup"
    This reverts commit 086ebfa8f27f015cb270580d5a8b0c1b5fdd5a27.

--- textpattern/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$txpcfg['db'] = 'cochran';
-$txpcfg['user'] = 'root';
-$txpcfg['pass'] = '';
-$txpcfg['host'] = 'localhost';
-$txpcfg['table_prefix'] = '';
-$txpcfg['txpath'] = 'C:\Users\computer\Documents\GitHub\cochran\textpattern';
-$txpcfg['dbcharset'] = 'utf8';
-?>
